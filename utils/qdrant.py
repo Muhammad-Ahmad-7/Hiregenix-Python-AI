28c761209b61142492e55e9cc00ac9129ff918f4
Muhammad-Ahmad-7 <2003ahmadwaheed@gmail.com>
2025-11-27 13:03:03 +0500

    qdrant live setup done

--- utils/qdrant.py
+++ utils/qdrant.py
@@ -5,7 +5,10 @@ from qdrant_client import QdrantClient
 
 def connection_qdrant():
     # --- Setup Qdrant DB ---
-    client = QdrantClient(host="localhost", port=6333)
+    client = QdrantClient(
+        url="https://3f23e5b1-e25d-4fab-85bc-ad8b524c921c.eu-central-1-0.aws.cloud.qdrant.io:6333", 
+        api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.RgwGRYz6Mwom_DHpizYb_5rZLzWWW6r5saV8bZ7Nq-g",
+    )
     return client
 
 def create_qdrant_collection(client, name: str):
