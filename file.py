21d8a0c64d00f4fc01b51cbe9b4c8cadba9bf9dc
Muhammad-Ahmad-7 <2003ahmadwaheed@gmail.com>
2026-01-27 17:20:26 +0500

    audio and video analysis worker queue boiler plate code done

--- file.py
+++ file.py
@@ -1,4 +1,10 @@
-from datetime import datetime
+import whisper
 
-current_time = datetime.now()
-print(f"Storing parsed data at {current_time}")
+
+def extract_stt(audio_path: str) -> str:
+    model = whisper.load_model("small")
+    result = model.transcribe(audio_path)
+    print(result)
+    return result["text"]
+
+extract_stt("https://res.cloudinary.com/hiregenx/video/upload/v1769427469/ivkiwf3limvam9fje7w0.mp3")
